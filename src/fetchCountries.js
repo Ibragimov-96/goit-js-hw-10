284985f335fbb9bf5390c23146b87031e0c178a6
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-11-04 12:30:02 +0200

    result

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,14 +1,10 @@
-import Notiflix from 'notiflix'
-export {fetchCountries}
-
-function fetchCountries(name){
-
-return fetch(`https://restcountries.com/v3.1/name/${name}?name.official,capital,population,flags.svg,languages`)
-    .then(responce => {
-        if(responce.ok){
-    return responce.json();}
+export function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
     throw new Error(response.statusText);
-        })
-        
-    
+  });
 }
--- src/index.js
+++ src/index.js
@@ -6,35 +6,30 @@ const DEBOUNCE_DELAY = 300;
 
 const refs = {
   cardContainer: document.querySelector('.country-info'),
-  countriesLisst: document.querySelector('#search-box'),
-  countri: document.querySelector('.country-list'),
+  countriesList: document.querySelector('#search-box'),
+  country: document.querySelector('.country-list'),
 };
-refs.countriesLisst.addEventListener('input', onSearch);
-refs.cardContainer.addEventListener('input', onCountries);
+refs.countriesList.addEventListener(
+  'input',
+  debounce(onSearch, DEBOUNCE_DELAY)
+);
 
 function onSearch(e) {
-  e.preventDefault();
-  const form = e.currentTarget;
-  const search = form.value;
-console.log(search)
+  const search = e.target.value.trim();
+
+  if (!search) return;
+  addMarkup();
   fetchCountries(search)
-    .then((responce = []) => {
-      const length = responce.length;
+    .then(response => {
+      const length = response.length;
+
       if (length === 1) {
-        refs.cardContainer.innerHTML = `
-    <img src="${responce[0].flags.svg}" style="width:25px">
-     <h1>${responce[0].name.official}</h1>
-      <p>${responce[0].capital}</p>
-      <p>${responce[0].population}</p>
-       <p>${responce[0].languages}</p>`;
-      } else if (length >= 2) {
-        Notiflix.Notify.success(
-          'Too many matches found. Please enter a more specific name.'
-        );
-        refs.countri.innerHTML = `
-<img src="${responce[0].flags.svg}">
- <h1>${responce[0].name.official}</h1>`;
-      } else if (length > 10) {
+        const markup = createMarkupCountry(response);
+        addMarkup(markup);
+      } else if (length > 1 && length <= 10) {
+        const markup = createMarkupListCountry(response);
+        addMarkup('', markup);
+      } else {
         Notiflix.Report.warning('Oops, there is no country with that name');
       }
     })
@@ -44,3 +39,28 @@ console.log(search)
     });
 }
 
+function addMarkup(cm = '', clm = '') {
+  refs.cardContainer.innerHTML = cm;
+  refs.country.innerHTML = clm;
+}
+
+function createMarkupCountry(data = []) {
+  return data
+    .map(({ flags, name, capital, population, languages }) => {
+      return `<div><img src="${flags.svg}" style="width:50px">
+    <h1>name ${name.official}</h1>
+     <p>${capital}</p>
+     <p>${population}</p>
+      <p>${Object.values(languages)[0]}</p></div>`;
+    })
+    .join('');
+}
+
+function createMarkupListCountry(data = []) {
+  return data
+    .map(({ flags, name }) => {
+      return `<div><img src="${flags.svg}" style="width:50px">
+      <h1>${name.official}</h1></div>`;
+    })
+    .join('');
+}
